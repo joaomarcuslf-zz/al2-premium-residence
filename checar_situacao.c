3bdbbb18cb48d627e868de89714eb53ce536174a
João Marcus de Lemos <jmpierrotfernandes@hotmail.com>
2015-09-29 11:36:34 -0300

    Adicionado Checar situacao

---
+++ checar_situacao.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define true 1
+#define false 0
+#define and &&
+
+int main() {
+  int checar;
+  int checar_situacao(int, int, int, char);
+  checar = checar_situacao(true, 405)
+}
+
+int checar_situacao(int aval, int num,, int bloco, char sit) {
+  if(aval==true and apartamento[bloco-1][(num/100)-1][num%10]=='D') {
+    apartamento[bloco-1][(num/100)-1][num%10]=sit;
+    return true;
+  }
+  else if(aval==false) {
+    return false;
+  }
+  else {
+
+  }
+
+}
